394ebc959fa2b8339fa8b0c4ccb4ded4daa8ac63
TayHobbs <taylor.hobbs@imtins.com>
2019-10-02 14:37:30 -0500

    Update tests to work with pytest

--- test/test_descript_ion_unit.py
+++ test/test_descript_ion_unit.py
@@ -5,69 +5,61 @@ Description:
 """
 
 import unittest
-import descript.ion
+from src.descript import ion
 import tempfile
 
 
-class DescriptIonTest(unittest.TestCase):
+tmp_file, tmp_filename = tempfile.mkstemp()
+tmp_file2, tmp_filename2 = tempfile.mkstemp()
+description = 'test description'
 
-    def setUp(self):
-        self.tmp_file, self.tmp_filename = tempfile.mkstemp()
-        self.tmp_file2, self.tmp_filename2 = tempfile.mkstemp()
-        self.description = 'test description'
+def test_descripions_as_dict():
+    descriptions = ion.Description()
+    descriptions[tmp_filename] = description
+    assert descriptions[tmp_filename] == description
 
-    def test_descripions_as_dict(self):
-        descriptions = descript.ion.Description()
-        descriptions[self.tmp_filename] = self.description
-        self.assertEqual(descriptions[self.tmp_filename], self.description)
+def test_read_write():
+    try:
+        f = ion.open(tmp_filename, 'r')
+        f.description = description
+        assert f.description == description
+    finally:
+        del f.description
+        f.close()
 
-    def test_read_write(self):
-        try:
-            f = descript.ion.open(self.tmp_filename, 'r')
-            f.description = self.description
-            self.assertEqual(f.description, self.description)
-        finally:
-            del f.description
-            f.close()
+def test_read_write_with_context():
+    with ion.open(tmp_filename) as f:
+        f.description = description
 
-    def test_read_write_with_context(self):
-        with descript.ion.open(self.tmp_filename) as f:
-            f.description = self.description
+    with ion.open(tmp_filename) as f:
+        assert f.description == description
+        del f.description
 
-        with descript.ion.open(self.tmp_filename) as f:
-            self.assertEqual(f.description, self.description)
-            del f.description
+def test_read_write_binary():
+    with ion.open(tmp_filename, 'wb') as f:
+        f.description = description
 
-    def test_read_write_binary(self):
-        with descript.ion.open(self.tmp_filename, 'wb') as f:
-            f.description = self.description
+    with ion.open(tmp_filename, 'rb') as f:
+        assert f.description == description
+        del f.description
 
-        with descript.ion.open(self.tmp_filename, 'rb') as f:
-            self.assertEqual(f.description, self.description)
-            del f.description
+def test_read_write_with_two_files():
+    try:
+        f = ion.open(tmp_filename, 'r')
+        f2 = ion.open(tmp_filename2, 'r')
+        f.description = description
+        f2.description = description+'2'
 
-    def test_read_write_with_two_files(self):
-        try:
-            f = descript.ion.open(self.tmp_filename, 'r')
-            f2 = descript.ion.open(self.tmp_filename2, 'r')
-            f.description = self.description
-            f2.description = self.description+'2'
+        assert f.description == description
+        assert f2.description == description+'2'
+    finally:
+        del f.description
+        del f2.description
+        f.close()
+        f2.close()
 
-            self.assertEqual(f.description, self.description)
-            self.assertEqual(f2.description, self.description+'2')
-        finally:
-            del f.description
-            del f2.description
-            f.close()
-            f2.close()
-
-    def test_read_write_with_file_with_spaces_in_name(self):
-        with descript.ion.open(self.tmp_filename+' test', 'a+') as f:
-            f.description = self.description
-            self.assertEqual(f.description, self.description)
-            del f.description
-
-
-if __name__ == '__main__':
-    tests = unittest.TestLoader().loadTestsFromTestCase(DescriptIonTest)
-    unittest.TextTestRunner(verbosity=2).run(tests)
+def test_read_write_with_file_with_spaces_in_name():
+    with ion.open(tmp_filename+' test', 'a+') as f:
+        f.description = description
+        assert f.description == description
+        del f.description
